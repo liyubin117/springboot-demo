43e7a8fc47158df75d76c048ab0c64d222c3aa2a
Yubin Li <lixin58688@163.com>
2022-01-17 11:55:56 +0800

    refactor

--- springboot-servlet/src/main/java/org/rick/servlet/TestServlet.java
+++ springboot-servlet/src/main/java/org/rick/servlet/TestServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/servlet2")
