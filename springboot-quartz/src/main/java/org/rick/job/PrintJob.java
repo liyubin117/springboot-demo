d8f53e8d0cd4569acda939c2ca3be9f69ccf63db
Yubin Li <lixin58688@163.com>
2022-01-18 17:39:59 +0800

    quartz: support exception handle and misfire

--- springboot-quartz/src/main/java/org/rick/job/PrintJob.java
+++ springboot-quartz/src/main/java/org/rick/job/PrintJob.java
@@ -1,7 +1,10 @@
 package org.rick.job;
 
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.PersistJobDataAfterExecution;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.text.SimpleDateFormat;
@@ -11,10 +14,26 @@ import java.util.Date;
  * @author Yubin Li
  * @date 2022/1/17 16:40
  **/
+@DisallowConcurrentExecution  //禁止并发执行(Quartz不要并发地执行同一个job定义（这里指一个job类的多个实例）)
+@PersistJobDataAfterExecution
+@Slf4j
 public class PrintJob extends QuartzJobBean {
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("current time :"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
-                + "---" + "scheduled dynamically!");
+        try {
+            Integer count = (Integer)jobExecutionContext.getJobDetail().getJobDataMap().get("count");
+            count++;
+            if (count > 5) {
+                throw new Exception();
+            }
+            jobExecutionContext.getJobDetail().getJobDataMap().put("count", count);
+            System.out.println("current time :"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
+                    + "---" + "scheduled dynamically!" + " " + count);
+        } catch (Exception e) {
+            log.error("cron job failed");
+            JobExecutionException e2 = new JobExecutionException(e);
+            e2.setUnscheduleAllTriggers(true);  //遇到执行异常后关掉此任务的所有trigger
+            throw e2;
+        }
     }
 }
--- springboot-quartz/src/main/java/org/rick/job/QuartzDynamic.java
+++ springboot-quartz/src/main/java/org/rick/job/QuartzDynamic.java
@@ -17,6 +17,7 @@ public class QuartzDynamic {
 
     /**
      * 创建定时任务
+     *
      * @param jobName
      * @param jobGroup
      * @param cron
@@ -26,35 +27,37 @@ public class QuartzDynamic {
      */
     //http://localhost:7022/create?jobName=task1&jobGroup=group1&cron=*/10 * * * * ?&triggerName=trigger1&triggerGroup=tGroup1
     @GetMapping("create")
-    public String createJob(String jobName,String jobGroup,String cron,String triggerName,String triggerGroup) throws SchedulerException {
+    public String createJob(String jobName, String jobGroup, String cron, String triggerName, String triggerGroup) throws SchedulerException {
 
         JobKey jobKey = new JobKey(jobName, jobGroup);
         // 如果存在这个任务，则删除
-        if(scheduler.checkExists(jobKey)) {
+        if (scheduler.checkExists(jobKey)) {
             scheduler.deleteJob(jobKey);
         }
 
         JobDetail jobDetail = JobBuilder.newJob(PrintJob.class)
-                                        .withIdentity(jobKey)
-                                        .build();
+                .withIdentity(jobKey)
+                .usingJobData("count", 0)
+                .build();
 
-        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(cron);
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(cron).withMisfireHandlingInstructionFireAndProceed();
 
         Trigger trigger = TriggerBuilder.newTrigger()
-                                        .withIdentity(triggerName,triggerGroup)
-                                        .withSchedule(cronScheduleBuilder).build();
-        scheduler.scheduleJob(jobDetail,trigger);
+                .withIdentity(triggerName, triggerGroup)
+                .withSchedule(cronScheduleBuilder).build();
+        scheduler.scheduleJob(jobDetail, trigger);
         return "job created!";
     }
 
     /**
      * 暂停定时任务
+     *
      * @param jobName
      * @param jobGroup
      * @throws SchedulerException
      */
     @GetMapping("pause")
-    public void pauseJob(String jobName,String jobGroup) throws SchedulerException {
+    public void pauseJob(String jobName, String jobGroup) throws SchedulerException {
 
         JobKey jobKey = JobKey.jobKey(jobName, jobGroup);
         JobDetail jobDetail = scheduler.getJobDetail(jobKey);
@@ -66,6 +69,7 @@ public class QuartzDynamic {
 
     /**
      * 恢复定时任务
+     *
      * @param jobName
      * @param jobGroup
      */
@@ -81,6 +85,7 @@ public class QuartzDynamic {
 
     /**
      * 删除定时任务
+     *
      * @param jobName
      * @param jobGroup
      * @param triggerName
@@ -88,11 +93,11 @@ public class QuartzDynamic {
      * @throws SchedulerException
      */
     @GetMapping("remove")
-    public String removeJob(String jobName, String jobGroup,String triggerName,String triggerGroup) throws SchedulerException {
+    public String removeJob(String jobName, String jobGroup, String triggerName, String triggerGroup) throws SchedulerException {
 
         TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroup);
         JobKey jobKey = JobKey.jobKey(jobName, jobGroup);
-        Trigger trigger =  scheduler.getTrigger(triggerKey);
+        Trigger trigger = scheduler.getTrigger(triggerKey);
         if (trigger == null) {
             return "trigger not exists!";
         }
